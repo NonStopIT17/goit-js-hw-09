b2f070fb9a9a6cccdc58f539b9a6cde8c5d4f1df
Katerina <118116963+NonStopIT17@users.noreply.github.com>
2023-04-12 00:39:02 +0300

    Home work 9
    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+const body = document.querySelector('body');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', onClickStart);
+btnStop.addEventListener('click', onClickStop);
+
+function onClickStart(ev) {
+  btnStart.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+  const IntervalStartID = setInterval(() => {
+    if (!btnStart.attributes.disabled) {
+      clearInterval(IntervalStartID);
+      return;
+    }
+    body.style.backgroundColor = getRandomHexColor();
+    console.log();
+  }, 1000);
+  console.log(IntervalStartID);
+}
+
+function onClickStop(ev) {
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', true);
+}
 
